44b46e63e7b2748dcf24ee21e2e9fcb918dc0926
connorbrown04 <139719213+connorbrown04@users.noreply.github.com>
2023-07-21 16:26:25 +0000

    Updated Optimizer form

--- public/js/projects-script.js
+++ public/js/projects-script.js
@@ -1,34 +1,47 @@
 let imgDownloads = {optimizer: {href: "", name: ""}};
+let loadingTextElements = (() => {
+  const elements = document.querySelectorAll(".loading-text");
+  const values = {};
+  for(let element of elements) {
+    values[element] = "inactive";
+  }
+  return values;
+})();
+
 
 async function uploadFile() {
-  const loadText = document.getElementById("loading-text");
-  loadText.classList.remove("d-none");
+  const loadText = document.getElementById("opti-loading-text");
   const uploadForm = document.querySelector("#optimizer-upload-form");
   let formData = new FormData(uploadForm); 
   if(!fileupload.files[0]) {
     loadText.innerText = "No File Provided";
     return;
-  } else {
-    loadText.innerText = "Optimizing...";
   }
   formData.append("fileuploadd", fileupload.files[0]);
-  const res = await fetch('/upload', {
+  const resPromise = fetch('/upload', {
     method: "POST", 
     body: formData
   });
-  
-  let filename = "[placeholder]";
-  for (var pair of res.headers.entries()) {
-    if(pair[0] == "filename") {
-      filename = pair[1];
+  loadingTextElements[loadText] = "active";
+
+  resPromise.then((res) => {
+    let filename = "[placeholder]";
+    for (var pair of res.headers.entries()) {
+      if(pair[0] == "filename") {
+        filename = pair[1];
+      }
     }
-  }
-  res.blob().then(data => {
-    imgDownloads.optimizer.href = window.URL.createObjectURL(data);
-    imgDownloads.optimizer.name = filename;
-    document.querySelector("#download-button").classList.remove("d-none");
-    loadText.innerText = "";
+    res.blob().then(data => {
+      imgDownloads.optimizer.href = window.URL.createObjectURL(data);
+      imgDownloads.optimizer.name = filename;
+      document.querySelector("#optimizer-download").classList.remove("d-none");
+      loadingTextElements[loadText] = "inactive"
+    });
   });
+
+  dotAnimation(loadText, "Optimizing");
+  
+  
 }
 
 function downloadFile(project) {
@@ -50,9 +63,9 @@ function updateOptions(elementID) {
   }
   if(elementID == "loss-less") {
     if(document.getElementById("loss-less").value == "lossless") {
-      document.getElementById("qual-container").classList.add("d-none");
+      document.getElementById("qual-container").classList.add("invisible");
     } else {
-      document.getElementById("qual-container").classList.remove("d-none");
+      document.getElementById("qual-container").classList.remove("invisible");
     }
   }
 }
@@ -60,4 +73,17 @@ function updateOptions(elementID) {
 function updateQual(val) {
   document.getElementById("qual-display").innerText = val;
 }
+
+async function dotAnimation(element, pretext) {
+  dotAnimationRecur(element, pretext, 0, 0);
+  element.innerText = "";
+}
+
+async function dotAnimationRecur(element, pretext, count, internalCount) {
+  if(loadingTextElements[element] == "inactive" || internalCount > 10000) return;
+  element.innerText = pretext + ".".repeat(count);
+  setTimeout(dotAnimationRecur, 800, element, pretext, (count + 1) % 4);
+}
+
+
 
